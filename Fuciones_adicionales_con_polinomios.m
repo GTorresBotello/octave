82a0cc9e668a62ef4a968ee47444f50ed3fc3c04
gtorres <gtorresbotello@gmail.com>
2022-03-24 05:54:57 -0600

    Trabajo funciones con polinomios

---
+++ Fuciones_adicionales_con_polinomios.m
@@ -0,0 +1,15 @@
+#   Fx = X3 +3x2 +8x +2
+fx = [1 3 8 2]
+#   gx = -3x3 +8x2 -2x +10
+gx =[-3 8 -2 10]
+
+# a)
+fxgx = conv (fx, gx)
+# b)
+derfx = polyder(fx)
+dergx = polyder(gx)
+# c)
+fxeval = polyval(fx, 3)
+# d)
+Fxeval = polyval(fx, 2)
+Gxeval = polyval(gx, 2)
