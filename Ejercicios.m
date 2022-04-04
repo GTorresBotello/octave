f72b260ddd306b5b28f4cb2e1d1b293489256995
gtorres <gtorresbotello@gmail.com>
2022-04-04 09:23:56 -0500

    new file:   Ejercicios.m

---
+++ Ejercicios.m
@@ -0,0 +1,24 @@
+#1.-Realizar una funcion que converta grados celcius a grados fahrenheit
+GradoCelcius = 50
+GradoFahrenheit = GradoCelcius * 1.8 +32
+
+#2.-Hacer una funcion que determine la distancia entre 2 puntos de coordenadas (X1,Y1)
+#   y (X2, Y2)
+
+
+#3.-Hacer una funcion que determine el mayor de 3 numeros a, b, c
+
+#4.-Realizar una funcion que devuelva la suma de los ekenebtos de una matriz
+
+#5.-Realiza una funcion que convierta grados sexagesimales
+
+#6.-Realiza una funcion que convierta radianes en grados sexagesimales
+
+#7.-Realizar una funcion que determina el promedio de los elementos contenidos en un vector
+
+#8.-Realiza la funcion que determine la desviacion estandar de los elementos contenidos en un vector
+
+#9.-Realizar una funcion que encuentre el valor mas pequeño dentro de los contenidos en un vector
+
+#10.-Realizar una funcion que reciba una matriz cuadrada y devuelva cero si la matriz
+#    es no singular y uno si la matriz es singular
