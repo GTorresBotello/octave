196e5739a4041bee472c38c93757e4b370dd84f2
“gtorresm <gtorresbotello@gmail.com>
2022-05-05 21:52:59 -0500

    modified:   numero_es_par.m

--- numero_es_par.m
+++ numero_es_par.m
@@ -2,10 +2,8 @@ x = input("Ingrese un numero : ");
 
 if(mod(x, 2))
 	disp('El numero es impar')
-else if(x == 0)
+elseif(x == 0)
 	disp('El numero es neutro')
 else
 	disp('El numero es par')
-	
-endif
 endif
